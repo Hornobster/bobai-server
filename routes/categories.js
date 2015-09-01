c9c07d7507e1ee52e025e8a44ed1ff62d37ff519
Carlo Vespa <vespac@usi.ch>
2015-09-01 19:45:07 +0200

    Fix getCategories query.

--- routes/categories.js
+++ routes/categories.js
@@ -7,7 +7,7 @@ var config = require('../config.js');
 var categories = {
     getCategories: function (req, res) {
         pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-            client.query('SELECT * FROM categories WHERE unused=0', function (err, result) {
+            client.query('SELECT id, name FROM categories WHERE unused=0', function (err, result) {
                 done();
                 if (err) {
                     console.error(err);
